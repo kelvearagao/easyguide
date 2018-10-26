af85d573a42f6ea909a57e26ef2a0800df3132a8
Marcelo Silva <marcelosilva.developer@gmail.com>
2018-10-26 15:05:25 -0300

    Add star icon

--- packages/icon/src/icons.js
+++ packages/icon/src/icons.js
@@ -1,7 +1,9 @@
 import StarWithMedal from './StarWithMedal'
+import Star from './Star'
 
 const icons = {
   'star-with-medal': StarWithMedal,
+  'star': Star,
 }
 
 export default icons
--- packages/icon/src/index.js
+++ packages/icon/src/index.js
@@ -1,4 +1,5 @@
 import Icon from './Icon'
 import StarWithMedal from './StarWithMedal'
+import Star from './Star'
 
-export {Icon as default, StarWithMedal}
+export {Icon as default, StarWithMedal, Star}
